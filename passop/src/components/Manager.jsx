76dcea9ae5322cb54b4e822faba1bf475393b17c
Areeb Ahmed <areebahmed.one@gmail.com>
2026-05-01 23:37:22 +0500

    worked further on manager.jsx

--- passop/src/components/Manager.jsx
+++ passop/src/components/Manager.jsx
@@ -5,26 +5,38 @@ const Manager = () => {
         <>
             {/* <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-size-[14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-77.5 w-77.5 rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div> */}
 
-            <div className="manager bg-slate-700 h-dvh">
+            <div className="manager bg-slate-600 h-dvh">
 
                 <div className="main-headline p-5 flex flex-col justify-center items-center text-white font-bold">
-                    <h1 className='text-3xl'>PassOP</h1>
+                    <h1 className='text-3xl'>
+                        <span className='text-slate-950 bold'>&lt;</span>
+                        <span>Pass</span>
+                        <span className='text-slate-950 bold'>OP</span>
+                        <span className='text-slate-950 bold'>/&gt;</span>
+                    </h1>
                     <p>Your own Password Manager</p>
                 </div>
 
                 <div className="inputs flex justify-center items-center flex-col gap-3 p-5">
                     <div className="url">
-                        <input className='bg-slate-300 border-4 border-slate-900 w-2xl px-2 py-1 rounded-full' type="text" placeholder='Enter website url...' />
+                        <input className='bg-slate-300 border-4 border-slate-900 w-full px-2 py-1 rounded-full' type="text" placeholder='Enter website url...' />
                     </div>
 
                     <div className="rest-inputs flex justify-center items-center gap-2">
-                        <input className='bg-slate-300 border-4 border-slate-900 w-1xl px-2 py-1 rounded-full' type="text" placeholder='Enter username...' />
-                        <input className='bg-slate-300 border-4 border-slate-900 w-1xl px-2 py-1 rounded-full' type="text" placeholder='Enter password...' />
+                        <input className='bg-slate-300 border-4 border-slate-900 w-full px-2 py-1 rounded-full' type="text" placeholder='Enter username...' />
+                        <input className='bg-slate-300 border-4 border-slate-900 w-full px-2 py-1 rounded-full' type="text" placeholder='Enter password...' />
+                        <span className='absolute right-134 cursor-pointer'>
+                            <img width={30} src="/public/eye.svg" alt="show-password" />
+                        </span>
                     </div>
 
                     <div className="save-btn">
-                        <button className='font-bold gap-2 flex justify-center items-center rounded-full p-2 cursor-pointer text-white bg-slate-800 hover:bg-slate-700 border-2 border-slate-300'>
-                            <img className='w-7 invert' src="/public/github.svg" alt="github-logo" />
+                        <button className='font-bold flex justify-center items-center rounded-full px-2 py-1 cursor-pointer text-white bg-slate-900 hover:bg-slate-900 hover:border-slate-900 border-2 border-slate-300'>
+                            <lord-icon
+                                src="https://cdn.lordicon.com/vjgknpfx.json"
+                                trigger="hover"
+                                style={{"width":"35px", "height":"35px"}}>
+                            </lord-icon>
                             Save
                         </button>
                     </div>
--- passop/src/components/Navbar.jsx
+++ passop/src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='flex justify-between text-white px-10 py-5 bg-slate-900'>
-            <div className="logo font-bold text-3xl">PassOP</div>
+            <div className="logo font-bold text-3xl">
+                <span>&lt;</span>
+                <span>Pass</span>
+                <span>OP</span>
+                <span>/&gt;</span>
+            </div>
 
             <div className="github-btn">
                 <button className='font-bold gap-2 flex justify-center items-center rounded-full p-2 cursor-pointer hover:bg-slate-800 border-2'>
